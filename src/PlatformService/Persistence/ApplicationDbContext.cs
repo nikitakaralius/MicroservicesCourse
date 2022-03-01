15fdfa869463e6e0f193b2e99cfa1e1e326a165c
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-01 19:07:02 +0300

    Created ApplicationDbContext

---
+++ src/PlatformService/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Platform> Platforms { get; init; } = null!;
+}
