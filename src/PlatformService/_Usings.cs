393379b0f77f5c5d7bab01330a9ffcca01668712
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-01 21:23:41 +0300

    Created Repository

--- src/PlatformService/_Usings.cs
+++ src/PlatformService/_Usings.cs
@@ -1,4 +1,5 @@
 ﻿global using PlatformService;
 global using PlatformService.Models;
 global using PlatformService.Persistence;
+global using PlatformService.Persistence.Repository;
 global using Microsoft.EntityFrameworkCore;
