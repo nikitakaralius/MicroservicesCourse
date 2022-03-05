1a7b5a564caec0b511571e1f88b11394a76507f3
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-05 17:24:28 +0300

    Added sync commands service call after new platforms was created

--- src/PlatformService/Controllers/PlatformsController.cs
+++ src/PlatformService/Controllers/PlatformsController.cs
@@ -5,9 +5,14 @@ public class PlatformsController : ControllerBase
 {
     private readonly IPlatformRepository _repository;
     private readonly IMapper _mapper;
+    private readonly ICommandDataClient _commandDataClient;
 
-    public PlatformsController(IPlatformRepository repository, IMapper mapper) =>
-        (_repository, _mapper) = (repository, mapper);
+    public PlatformsController(IPlatformRepository repository, IMapper mapper, ICommandDataClient commandDataClient)
+    {
+        _repository = repository;
+        _mapper = mapper;
+        _commandDataClient = commandDataClient;
+    }
 
     [HttpGet]
     public ActionResult<IEnumerable<PlatformToRead>> AllPlatforms() =>
@@ -22,7 +27,7 @@ public class PlatformsController : ControllerBase
     }
 
     [HttpPost]
-    public ActionResult<PlatformToRead> Create(PlatformToCreate? platformToCreate)
+    public async Task<ActionResult<PlatformToRead>> Create(PlatformToCreate? platformToCreate)
     {
         if (platformToCreate is null)
             return BadRequest();
@@ -30,6 +35,16 @@ public class PlatformsController : ControllerBase
         _repository.Create(platform);
         _repository.SaveChanges();
         PlatformToRead platformToRead = _mapper.Map<PlatformToRead>(platform);
+
+        try
+        {
+            await _commandDataClient.SendPlatformToCommandAsync(platformToRead);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"==> Could not send synchronously: {e.Message}");
+        }
+
         return CreatedAtRoute(nameof(PlatformBy), new {Id = platformToRead.Id}, platformToRead);
     }
 }
