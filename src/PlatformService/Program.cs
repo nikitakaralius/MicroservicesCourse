ea66816b5525d1714f03fe07dbd3ba03595ea8e4
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-08 19:00:13 +0400

    Added SqlServer db in Production env

--- src/PlatformService/Program.cs
+++ src/PlatformService/Program.cs
@@ -2,7 +2,7 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 Console.WriteLine($"==>Using CommandService Endpoint: {app.Configuration.CommandsService()}");
