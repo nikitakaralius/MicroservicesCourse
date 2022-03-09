3255956ca32a0477bc425ceea3a7c2ce79c12f1b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-09 17:20:11 +0400

    Created Command Repository interface

---
+++ src/CommandsService/Persistence/Repository/ICommandRepository.cs
@@ -0,0 +1,14 @@
+namespace CommandsService.Persistence.Repository;
+
+public interface ICommandRepository
+{
+    bool SaveChanges();
+
+    IEnumerable<Platform> AllPlatforms();
+    bool PlatformExists(int id);
+    void Create(Platform platform);
+
+    IEnumerable<Command> CommandsForPlatform(int platformId);
+    Command CommandBy(int platformId, int commandId);
+    void Create(int platformId, Command command);
+}
