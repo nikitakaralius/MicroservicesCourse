f927d35b074cccdbbce4ac66a4208c3e87a800e2
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-09 17:22:51 +0400

    Refactored Command class to a record

--- src/CommandsService/Models/Command.cs
+++ src/CommandsService/Models/Command.cs
@@ -1,14 +1,3 @@
 namespace CommandsService.Models;
 
-public class Command
-{
-    public int Id { get; init; }
-
-    public string HowTo { get; init; } = null!;
-
-    public string Line { get; init; } = null!;
-
-    public int PlatformId { get; init; }
-
-    public Platform Platform { get; init; } = null!;
-}
+public record Command(int Id, string HowTo, string Line, int PlatformId, Platform Platform);
