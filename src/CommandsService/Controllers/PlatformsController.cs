541415805a309c9434e652a74ab504aa5beb015e
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-10 18:33:53 +0400

    Removed InboundTest endpoint in the PlatformsController

--- src/CommandsService/Controllers/PlatformsController.cs
+++ src/CommandsService/Controllers/PlatformsController.cs
@@ -19,11 +19,4 @@ public class PlatformsController : ControllerBase
         IEnumerable<PlatformTorRead> platformsTorRead = _mapper.Map<IEnumerable<PlatformTorRead>>(platforms);
         return Ok(platformsTorRead);
     }
-
-    [HttpPost]
-    public IActionResult TestInboundConnection()
-    {
-        Console.WriteLine("==> Inbound POST # Command service");
-        return Ok("Inbound test from Platforms Controller");
-    }
 }
